b155deac268793c4e0e4af4d0f465b46f05f0333
Nicholas Fitzhugh <nicholas@fitzhugh.dk>
2023-11-29 09:56:58 +0100

    Adding comments to function

--- TotalEffect.R
+++ TotalEffect.R
@@ -1,12 +1,15 @@
 TotalEffect <- function(t1,t2,net,effect){
   
+  # Extracting estimates from NMA
   tot.TE <- get(paste0('TE.', effect), net)[t1,t2]
   tot.seTE <- get(paste0('seTE.',effect), net)[t1,t2]
   tot.lower <- get(paste0('lower.', effect), net)[t1,t2]
   tot.upper <- get(paste0('upper.', effect), net)[t1,t2]
-
+  
+  # Determining if measure of effect needs transformation
   sm <- net$sm
   
+  # Setting outputs
   if(sm == 'HR' | sm == 'OR' | sm == 'RR'){
     out <- data.frame(t1,
                       t2, 
@@ -37,5 +40,7 @@ TotalEffect <- function(t1,t2,net,effect){
                       tot.upper, 
                       tot.TE)
   }
-  return(out)
+  
+  # Output
+  out
 }
